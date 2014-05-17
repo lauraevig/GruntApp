1c45889f7aba459610d62f78cf7ec0ec22acf1e1
Laura Vig <lauraevig@gmail.com>
2014-05-17 13:25:11 -0700

    package installs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Hey', name:'Laura' });
+});
+router.get('/foo', function(req, res) {
+  res.render('foo', { title: 'Hey', name:'Laura' });
 });
 
 module.exports = router;
